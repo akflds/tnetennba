86ccd24ff87fee5c640e22fb86c3c723543b2c01
akflds <andrewkirsfelds@gmail.com>
2022-04-18 20:07:05 +0100

    add starter functions and todolist

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const fs = require('fs')
 const wordListPath = require("word-list");
-const wordArray = fs.readFileSync(wordListPath, 'utf-8').split('\n').filter(word => {
-	return word.length >= 5 && word.length <= 8;
-});
 
-const eightLetterWords = wordArray.filter(word => word.length === 8);
+function getWordArray(path){
+	return fs.readFileSync(path, 'utf-8')
+	.split('\n')
+	.filter(word => { 
+		return word.length >= 5 && word.length <= 8;
+	});
+}
 
-console.log(wordArray.length);
-console.log(eightLetterWords.length);
-const randomIndex = Math.floor(Math.random() * eightLetterWords.length);
-console.log(eightLetterWords[randomIndex])
+function getStarterWord(arr){
+	const eightLetterWords = arr.filter(word => word.length === 8)
+	return eightLetterWords[Math.floor(Math.random() * eightLetterWords.length)];
+}
+
+/* 
+
+TODO:
+
+* Shuffle starter word
+* Find possible fives, sixes, sevens and eights
+* Read and store input
+	* Convert to lower 
+* Validate guess against relevant list
+	* Log any correct guesses
+	* Award points if not duplicate 
+	* Point scoring 1, 2, 4, 8 for 5-8 letters respectively, bonus for getting starter word
+* Toggle for total % of possible words/possible words by length
+* Add timer
+
+*/
