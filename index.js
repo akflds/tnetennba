c0bc1c7217affc1bafcfa220451136ff18109eeb
akflds <andrewkirsfelds@gmail.com>
2022-05-23 21:51:16 +0100

    setup front end and add buttons to generate letters/reset the game

--- index.js
+++ index.js
@@ -1,47 +1,63 @@
-const fs = require('fs')
-const wordListPath = require("word-list");
+const consonants = "bcdfghjklmnpqrstvwxyz";
+const vowels = "aeiou";
 
-function getPossibleWords(){
-	return fs.readFileSync(wordListPath, 'utf-8')
-	.split('\n')
-	.filter(word => { 
-		return word.length >= 4 && word.length <= 8;
-	});
-}
+const getRandomLetter = (str) => {
+  return str.split("")[Math.floor(Math.random() * str.length)];
+};
 
-function getStarterWord(words){
-	const eightLetterWords = words.filter(word => word.length === 8)
-	return eightLetterWords[Math.floor(Math.random() * eightLetterWords.length)];
-}
+const clearBoard = () => {
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    while (box.firstChild) {
+      box.removeChild(box.firstChild);
+    }
+    document.getElementById("consonant-btn").disabled = false;
+    document.getElementById("vowel-btn").disabled = false;
+  });
+};
 
-/* 
+const updateBoard = (letters) => {
+  const boxes = [...document.querySelectorAll(".box")];
+  // so bad!
+  console.log(boxes);
+  for (let i = 0; i < boxes.length; i++) {
+    if (letters[i]) {
+      if (boxes[i].childNodes.length === 0) {
+        const tile = document.createElement("p");
+        tile.innerText = letters[i];
+        boxes[i].appendChild(tile);
+      }
+    }
+  }
+  if (letters.length === 9) {
+    document.getElementById("consonant-btn").disabled = true;
+    document.getElementById("vowel-btn").disabled = true;
+  }
+};
 
-TODO:
+const initGame = () => {
+  const consonantBtn = document.getElementById("consonant-btn");
+  const vowelBtn = document.getElementById("vowel-btn");
+  const resetBtn = document.getElementById("reset-btn");
 
-* Shuffle starter word
-* Find possible fives, sixes, sevens and eights
-* Read and store input
-	* Convert to lower 
-* Validate guess against relevant list
-	* Log any correct guesses
-	* Award points if not duplicate 
-	* Point scoring 1, 2, 4, 8 for 5-8 letters respectively, bonus for getting starter word
-* Toggle for total % of possible words/possible words by length
-* Add timer
+  const letters = [];
 
-*/
+  consonantBtn.addEventListener("click", () => {
+    const letter = getRandomLetter(consonants);
+    letters.push(letter);
+    updateBoard(letters);
+  });
 
-const subSeq = (s) => s.length == 1
-    ? ['', s]
-    : (ss=subSeq(s.slice(1)),
-       [...ss.map(ch => s[0] + ch), ...ss])
+  vowelBtn.addEventListener("click", () => {
+    const letter = getRandomLetter(vowels);
+    letters.push(letter);
+    updateBoard(letters);
+  });
 
+  resetBtn.addEventListener("click", () => {
+    letters.length = 0;
+    clearBoard();
+  });
+};
 
-const allWords = getPossibleWords();
-const starter = getStarterWord(allWords);
-console.log(starter)
-
-const combos = subSeq(starter).filter((word) => allWords.includes(word));
-
-console.log(combos.length)
-console.log(combos);
+initGame();
