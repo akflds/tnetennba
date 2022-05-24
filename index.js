954e233097123de1900ff171a9a5c3fbc8b83816
akflds <andrewkirsfelds@gmail.com>
2022-05-24 09:00:46 +0100

    add letter distribution for vowels and consts - needs refactoring

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-const consonants = "bcdfghjklmnpqrstvwxyz";
-const vowels = "aeiou";
+const distVowels = {
+  A: 15,
+  E: 21,
+  I: 13,
+  O: 13,
+  U: 5,
+};
+
+const distConsts = {
+  B: 2,
+  C: 3,
+  D: 6,
+  F: 2,
+  G: 3,
+  H: 2,
+  J: 1,
+  K: 1,
+  L: 5,
+  M: 4,
+  N: 8,
+  P: 4,
+  Q: 1,
+  R: 9,
+  S: 9,
+  T: 9,
+  V: 1,
+  W: 1,
+  X: 1,
+  Y: 1,
+  Z: 1,
+};
+
+const consonants = Object.keys(distConsts)
+  .map((letter) => {
+    return letter.repeat(distConsts[letter]).split("");
+  })
+  .flat()
+  .sort(() => Math.random() - 0.5)
+  .join("");
+
+const vowels = Object.keys(distVowels)
+  .map((letter) => {
+    return letter.repeat(distVowels[letter]).split("");
+  })
+  .flat()
+  .sort(() => Math.random() - 0.5)
+  .join("");
 
 const getRandomLetter = (str) => {
   return str.split("")[Math.floor(Math.random() * str.length)];
